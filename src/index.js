a491df9543e43e5dc55905955e9c03814d9dcf04
TheFutaBandit <prameshanand2021@gmail.com>
2024-10-01 02:13:40 +0530

    styling and html, modal left

--- src/index.js
+++ src/index.js
@@ -7,3 +7,10 @@ const date = new Date()
 const dateFormattedd = format(date, "MM/dd/yyyy")
 console.log(dateFormattedd);
 
+const addButton = document.querySelector(".add-task");
+const modal = document.querySelector(".modal");
+
+addButton.addEventListener("click", () => {
+    modal.showModal();
+})
+
