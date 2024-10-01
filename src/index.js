38711cebfbf7bcc135c78e993d3503262608523d
TheFutaBandit <prameshanand2021@gmail.com>
2024-10-01 18:26:51 +0530

    modal Changes

--- src/index.js
+++ src/index.js
@@ -8,9 +8,32 @@ const dateFormattedd = format(date, "MM/dd/yyyy")
 console.log(dateFormattedd);
 
 const addButton = document.querySelector(".add-task");
+const expandButton = document.querySelector(".check")
 const modal = document.querySelector(".modal");
+// const taskExpand = document.querySelector("#task-dialog")
+const closeExpand = document.querySelector(".close-expand")
+
 
 addButton.addEventListener("click", () => {
     modal.showModal();
 })
+
+
+const taskDialog = document.getElementById('task-dialog');
+  
+  // Function to open the modal
+  function openTaskModal() {
+      taskDialog.showModal();
+  }
+
+  // Function to close the modal
+  function closeTaskModal() {
+      taskDialog.close();
+  }
+
+  // Add event listener to the task button to open the modal
+  document.querySelector('.check').addEventListener('click', openTaskModal);
+
+  // Add event listener to the close button inside the modal
+  document.querySelector('.close-expand').addEventListener('click', closeTaskModal);
 
